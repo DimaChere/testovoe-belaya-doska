5b3ecb31a1a60a2358525483177d78105d344846
DimaChere <dmitriy.cherepanov02@mail.ru>
2024-08-18 13:30:50 +0600

    const to useRef

--- src/app/tester/page.tsx
+++ src/app/tester/page.tsx
@@ -6,43 +6,58 @@ interface Item {
     y: number;
     radius: number;
     speed: number;
+
     direction: number;
+    color: string;
 }
 
 export default function Tester() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const itemsRef = useRef<Item[]>([
+        {
+            x: 200,
+            y: 200,
+            radius: 50,
+            speed: 3,
+            direction: 1,
+            color: "blue",
+        },
+        {
+            x: 300,
+            y: 100,
+            radius: 20,
+            speed: 5,
+            direction: 1,
+            color: "red",
+        },
+    ]);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas?.getContext("2d");
         if (!ctx || !canvas) return;
 
-        let item: Item = {
-            x: 200,
-            y: 200,
-            radius: 50,
-            speed: 3,
-            direction: 1,
-        };
-
         const animate = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            item.y += item.speed * item.direction;
+            const items = itemsRef.current;
+            items.forEach((item) => {
+                item.y += item.speed * item.direction;
 
-            if (
-                item.y + item.radius > canvas.height ||
-                item.y - item.radius < 0
-            ) {
-                item.direction *= -1;
-            }
+                if (
+                    item.y + item.radius > canvas.height ||
+                    item.y - item.radius < 0
+                ) {
+                    item.direction *= -1;
+                }
 
-            ctx.beginPath();
-            ctx.arc(item.x, item.y, item.radius, 0, Math.PI * 2);
-            ctx.fillStyle = "blue";
-            ctx.fill();
-            ctx.stroke();
-            ctx.closePath();
+                ctx.beginPath();
+                ctx.arc(item.x, item.y, item.radius, 0, Math.PI * 2);
+                ctx.fillStyle = item.color;
+                ctx.fill();
+                ctx.stroke();
+                ctx.closePath();
+            });
 
             requestAnimationFrame(animate);
         };

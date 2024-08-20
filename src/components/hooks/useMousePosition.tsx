0a1f84e488eedf95fc2bd712d21eb4c21b103888
DimaChere <dmitriy.cherepanov02@mail.ru>
2024-08-20 09:47:04 +0600

    mouse moves hook

---
+++ src/components/hooks/useMousePosition.tsx
@@ -0,0 +1,19 @@
+import { useRef, useEffect } from "react";
+
+const useMousePosition = () => {
+    const mousePositionRef = useRef<{
+        x: number;
+        y: number;
+    }>({ x: 0, y: 0 });
+    useEffect(() => {
+        const updateMousePosition = (ev: MouseEvent) => {
+            mousePositionRef.current = { x: ev.clientX, y: ev.clientY };
+        };
+        window.addEventListener("mousemove", updateMousePosition);
+        return () => {
+            window.removeEventListener("mousemove", updateMousePosition);
+        };
+    }, []);
+    return mousePositionRef;
+};
+export default useMousePosition;
